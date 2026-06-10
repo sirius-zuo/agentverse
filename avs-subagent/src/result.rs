ff6b93f9b355388b44a2e902c02136e2d5b5f478
Jin Zuo <zuojin@gmail.com>
2026-06-10 16:58:19 -0400

    fix(subagent): use workspace httpmock dep and derive Error for SubAgentError stub

--- avs-subagent/src/result.rs
+++ avs-subagent/src/result.rs
@@ -1,5 +1,6 @@
-// TODO
-
-pub type SubAgentResult<T> = Result<T, SubAgentError>;
-
+#[derive(Debug, thiserror::Error)]
+#[error("subagent error")]
 pub struct SubAgentError;
+
+#[allow(dead_code)]
+pub type SubAgentResult<T> = Result<T, SubAgentError>;
